22a6f2c345537e3d183ee0cef05fa70f65b3386d
Ryan Farnell <rfarnell@fastmail.com>
2023-12-01 09:54:55 -0600

    Finished part 1.

--- cmd/day1/main.go
+++ cmd/day1/main.go
@@ -3,16 +3,57 @@ package main
 import (
 	"fmt"
 	"log"
+	"slices"
+	"strconv"
+	"strings"
 
 	"github.com/criscokid/aoc2023/internal/fileinput"
 )
 
+var numberRunes = []rune{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+
 func main() {
-	file_path := "small_input.txt"
+	file_path := "input.txt"
 	lines, err := fileinput.ReadLines(file_path)
 	if err != nil {
 		log.Fatal(err)
-	} 
+	}
 
-	fmt.Print(lines)
+	values := make([]int, 0)
+
+	for _, line := range lines {
+		numbers := findNumbers(line)
+		numVal, err := getNumberFromLineRunes(numbers)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		values = append(values, numVal)
+	}
+	fmt.Println(sum(values))
+
+}
+
+func findNumbers(input string) []rune {
+	numbers := make([]rune, 0)
+	for _, c := range input {
+		if slices.Contains(numberRunes, c) {
+			numbers = append(numbers, c)
+		}
+	}
+	return numbers
+}
+
+func getNumberFromLineRunes(numbers []rune) (int, error) {
+	var sb strings.Builder
+	sb.WriteRune(numbers[0])
+	sb.WriteRune(numbers[len(numbers)-1])
+	return strconv.Atoi(sb.String())
+}
+
+func sum(values []int) int {
+	total := 0
+	for _, n := range values {
+		total = total + n
+	}
+	return total
 }
